d027d834a298e727319e752acb964867104e08e6
qkdtkrl2 <56636585+qkdtkrl2@users.noreply.github.com>
2021-02-23 22:36:43 +0900

    2021.02.23(Thu)  ㄴQR 바코드 스캐너 실행

--- app/src/main/java/com/example/lotto/MainActivity.java
+++ app/src/main/java/com/example/lotto/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.lotto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -35,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
     String str, receiveMsg;
 
+    IntentIntegrator integrator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +56,19 @@ public class MainActivity extends AppCompatActivity {
         btnCreateNum = findViewById(R.id.btnCreateNum);
         btnMyNum = findViewById(R.id.btnMyNum);
         btnMap = findViewById(R.id.btnMap);
+
+        integrator = new IntentIntegrator(this);
+        //바코드 안의 텍스트
+        integrator.setPrompt("바코드를 사각형 안에 비춰주세요");
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(true);
+
+        btnQr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                integrator.initiateScan();
+            }
+        });
 
         CountTask count = new CountTask();
         count.execute();
